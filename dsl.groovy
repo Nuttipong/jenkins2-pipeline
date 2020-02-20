732b080efd6bd2a8f8b2267cd4da01ce8bf2ace7
nuttipong.t <nuttipongg@gmail.com>
2020-02-20 10:55:08 +0700

    feat: modify script

--- dsl.groovy
+++ dsl.groovy
@@ -1,30 +1,77 @@
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.*
 
+def giturl = 'https://github.com/Nuttipong/jenkins2-pipeline.git'
+def jobs = [
+  name: 'job-dsl-1', 
+  name: 'job-dsl-2'
+]
+def started = new Date()
 
-// def started = new Date()
+final header = "hxl_maint_4.0_"
+def jobs = [:];
+
+jobs.each {
+  job -> jobs[job.name] = {
+    new BaseJobBuilder(
+      name: header + job.name
+      description: 'test'
+    ).build(this)
+  }
+}
+
+parallel jobs
+
+class BaseJobBuilder {
+    String name
+    String description
+
+    Job build(DslFactory factory) {
+        factory.pipelineJob(this.name) {
+            it.description this.description
+        }
+    }
+}
+
+// class CommonUtils {
+//   static void addDefaults(context) {
+//       context.with {
+//           wrappers {
+//               colorizeOutput()
+//               timestamps()
+//           }
+//           logRotator {
+//               numToKeep(100)
+//           }
+//           publishers {
+//               allowBrokenBuildClaiming()
+//           }
+//       }
+//   }
+// }
 
 // parallel firstBranch: {
-//         // do something
-//         println 'Hello 1'
 //         println new Date() - started
 //     }, secondBranch: {
-//         // do something else
-//         println 'Hello 2'
+
 //         println new Date() - started
 //     },
 //     failFast: true
+// }
 
-pipelineJob('job-dsl-plugin') {
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url('https://github.com/Nuttipong/jenkins2-pipeline.git')
-          }
-          branch('*/master')
-        }
-      }
-      lightweight()
-    }
-  }
-}
+// pipelineJob('job-dsl-plugin') {
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//             url(giturl)
+//           }
+//           branch('*/master')
+//         }
+//       }
+//       lightweight()
+//     }
+//   }
+// }
