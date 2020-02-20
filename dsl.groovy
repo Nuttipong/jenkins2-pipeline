1f988eb0c9531656c62caeb46cde2ee78994a9b9
nuttipong.t <nuttipongg@gmail.com>
2020-02-20 16:59:40 +0700

    feat: modify script

--- dsl.groovy
+++ dsl.groovy
@@ -11,133 +11,133 @@ def jobs = [
 
 Set ia_versions = ['4.0.1', '4.0.0']
 
-multiJob('build hxl jobs') {
-    steps {
-        phase('portal') {
+// multiJob('build hxl jobs') {
+//     steps {
+//         phase('portal') {
           
-        }
-        phase('ia') {
+//         }
+//         phase('ia') {
 
-        }
-        phase('backend') {
+//         }
+//         phase('backend') {
 
+//         }
+//     }
+// }
+
+
+abstract class BaseJobBuilder {
+    private String name
+    private String description
+    private def job
+
+    def build() {
+      job(this.name) {
+        logRotator {
+          numToKeep(numbBuildToKeep)
         }
+      }
     }
 }
 
+interface CreateJob {
+  void createJob(String name)
+}
 
-// abstract class BaseJobBuilder {
-//     private String name
-//     private String description
-//     private def job
+interface AddChoiceParam {
+  void addChoiceParam(String param1, String[] param2, String param3 = '')
+}
 
-//     def build() {
-//       job(this.name) {
-//         logRotator {
-//           numToKeep(numbBuildToKeep)
-//         }
-//       }
-//     }
-// }
+interface AddConfig {
+  void addConfig(String sortMode)
+}
 
-// interface CreateJob {
-//   void createJob(String name)
-// }
+interface AddStringParam {
+  void addStringParam(String param1 = '', String param2 = '', String param3 = '')
+}
 
-// interface AddChoiceParam {
-//   void addChoiceParam(String param1, String[] param2, String param3 = '')
-// }
+interface AddDefinition {
+  void addDefinition(String repo, String branch)
+}
 
-// interface AddConfig {
-//   void addConfig(String sortMode)
-// }
+class Job extends BaseJobBuilder implements CreateJob, AddChoiceParam, AddConfig, AddStringParam, AddDefinition {
 
-// interface AddStringParam {
-//   void addStringParam(String param1 = '', String param2 = '', String param3 = '')
-// }
+  private def job = null
 
-// interface AddDefinition {
-//   void addDefinition(String repo, String branch)
-// }
+  void createJob(String name, String description) {
+    job = new BaseJobBuilder(
+      name: name,
+      description: description
+    ).build()
+  }
 
-// class Job extends BaseJobBuilder implements CreateJob, AddChoiceParam, AddConfig, AddStringParam, AddDefinition {
+  void addChoiceParam(String param1, String[] param2, String param3 = '') {
+    job.with = {
+      choiceParam(param1, [param2], param3)
+    }
+  }
 
-//   private def job = null
+  void addConfig(String sortMode) {
+    job.with = {
+      configure {
+          it / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' / 'net.uaznia.lukanus.hudson.plugins.gitparameter.GitParameterDefinition' {
+          name ('DEFAULT_BRANCH')
+          description ("Default branch to be used. This option will be only for BUILD_LATEST.")
+          type ('PT_BRANCH')
+          branchFilter ('origin/release.*')
+          sortMode (sortMode)
+          selectedValue('TOP')
+          listSize('5')
+        }
+      }
+    }
+  }
 
-//   void createJob(String name, String description) {
-//     job = new BaseJobBuilder(
-//       name: name,
-//       description: description
-//     ).build()
-//   }
+  void addStringParam(String param1 = '', String param2 = '', String param3 = '') {
+    job.with = {
+      stringParam(param1, param2, param3)
+    }
+  }
 
-//   void addChoiceParam(String param1, String[] param2, String param3 = '') {
-//     job.with = {
-//       choiceParam(param1, [param2], param3)
-//     }
-//   }
+  void addDefinition(String repo, String branch, boolean lightweight) {
+    job.with = {
+      definition {
+          cpsScm {
+              scm {
+                  git {
+                      remote {
+                          github(repo, "https", "github.developer.allianz.io")
+                          credentials("git-token-credentials")
+                      }
+                      branch(branch)
+                      extensions{
+                          wipeOutWorkspace()
+                          cloneOptions {
+                              depth(2)
+                              honorRefspec(false)
+                              shallow(true)
+                              noTags(true)
+                              timeout(10)
+                          }
+                      }
+                  }
+                  scriptPath(feJenkinsfile)
+                  lightweight(lightweight)
+              }
+          }
+      }
+    }
+  }
 
-//   void addConfig(String sortMode) {
-//     job.with = {
-//       configure {
-//           it / 'properties' / 'hudson.model.ParametersDefinitionProperty' / 'parameterDefinitions' / 'net.uaznia.lukanus.hudson.plugins.gitparameter.GitParameterDefinition' {
-//           name ('DEFAULT_BRANCH')
-//           description ("Default branch to be used. This option will be only for BUILD_LATEST.")
-//           type ('PT_BRANCH')
-//           branchFilter ('origin/release.*')
-//           sortMode (sortMode)
-//           selectedValue('TOP')
-//           listSize('5')
-//         }
-//       }
-//     }
-//   }
+}
 
-//   void addStringParam(String param1 = '', String param2 = '', String param3 = '') {
-//     job.with = {
-//       stringParam(param1, param2, param3)
-//     }
-//   }
-
-//   void addDefinition(String repo, String branch, boolean lightweight) {
-//     job.with = {
-//       definition {
-//           cpsScm {
-//               scm {
-//                   git {
-//                       remote {
-//                           github(repo, "https", "github.developer.allianz.io")
-//                           credentials("git-token-credentials")
-//                       }
-//                       branch(branch)
-//                       extensions{
-//                           wipeOutWorkspace()
-//                           cloneOptions {
-//                               depth(2)
-//                               honorRefspec(false)
-//                               shallow(true)
-//                               noTags(true)
-//                               timeout(10)
-//                           }
-//                       }
-//                   }
-//                   scriptPath(feJenkinsfile)
-//                   lightweight(lightweight)
-//               }
-//           }
-//       }
-//     }
-//   }
-
-// }
-
-// def job = new Job()
-// job.createJob(jobs['portal'][0], jobs['portal'][1])
-// job.addChoiceParam('ENVIRONMENT', [space, space + '@AWS'], '')
-// job.addConfig('DESCENDING')
-// job.addChoiceParam('M_APP_VERSION', ia_versions, 'tell Code-Push apply to which mobile package version')
-// job.addChoiceParam('BUILD_OPTIONS', 
-//   ['BUILD_FROM_SIT_TAG','BUILD_FROM_UAT_TAG', 'BUILD_FROM_TAG', 'BUILD_FROM_BRANCH', 'DELETE_TAG'], '')
-// job.addStringParam('BUILD_SPECIFIER', '', 'version number of SIT or UAT or MAINT tag, or branch name')
-// job.addStringParam('COMMIT_ID', '', 'BUILD_FROM_COMMIT_ID or MAKE_TAG_ONLY (MAKE_TAG_ONLY -> will make a tag with this commit id)')
-// job.addDefinition(addDefinition('hexalite/provider_portal', 'refs/remotes/\${defaultBranch}', false)
+def job = new Job()
+job.createJob(jobs['portal'][0], jobs['portal'][1])
+job.addChoiceParam('ENVIRONMENT', [space, space + '@AWS'], '')
+job.addConfig('DESCENDING')
+job.addChoiceParam('M_APP_VERSION', ia_versions, 'tell Code-Push apply to which mobile package version')
+job.addChoiceParam('BUILD_OPTIONS', 
+  ['BUILD_FROM_SIT_TAG','BUILD_FROM_UAT_TAG', 'BUILD_FROM_TAG', 'BUILD_FROM_BRANCH', 'DELETE_TAG'], '')
+job.addStringParam('BUILD_SPECIFIER', '', 'version number of SIT or UAT or MAINT tag, or branch name')
+job.addStringParam('COMMIT_ID', '', 'BUILD_FROM_COMMIT_ID or MAKE_TAG_ONLY (MAKE_TAG_ONLY -> will make a tag with this commit id)')
+job.addDefinition(addDefinition('hexalite/provider_portal', 'refs/remotes/\${defaultBranch}', false)
