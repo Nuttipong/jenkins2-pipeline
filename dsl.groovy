1f105ab19b02e43103bf2990d7aac245f688a18c
nuttipong.t <nuttipongg@gmail.com>
2020-02-21 00:09:29 +0700

    feat: modify scriptclear

--- dsl.groovy
+++ dsl.groovy
@@ -218,9 +218,10 @@ def tasks = [
 tasks.values().each {
   task -> 
       def pipeline = pipelineJob(task[0])
-      def job
+      
       if (task[2] == Web) {
-        job = new Web(pipeline)
+        def job = new Web(pipeline)
+        job.build()
       }
       // if (task[2] == Mobile) {
       //   job = new Mobile(pipeline)
