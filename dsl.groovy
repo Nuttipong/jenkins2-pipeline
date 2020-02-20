6d7be0e9c07c97c22661254c0e054a8f35ba5118
nuttipong.t <nuttipongg@gmail.com>
2020-02-20 12:38:06 +0700

    feat: modify script

--- dsl.groovy
+++ dsl.groovy
@@ -7,32 +7,45 @@ def jobs = [
   'job-dsl-2'
 ]
 def tasks = [:]
-
-jobs.each {
-  name -> tasks[name] = {
-    // node {
-    //   new BaseJobBuilder(
-    //     name: header + job,
-    //     description: 'test'
-    //   ).build()
-    // }
-    pipelineJob(name) {
-      definition {
-        cpsScm {
-          scm {
-            git {
-              remote {
-                url(giturl)
-              }
-              branch('*/master')
-            }
-          }
-          lightweight()
-        }
-      }
+tasks["task_1"] = {
+  stage ("task_1"){    
+    node('label_example1') {  
+        sh 'echo $NODE_NAME'
     }
   }
 }
+tasks["task_2"] = {
+  stage ("task_2"){    
+    node('label_example2') {  
+        sh 'echo $NODE_NAME'
+    }
+  }
+}
+// jobs.each {
+//   name -> tasks[name] = {
+//     // node {
+//     //   new BaseJobBuilder(
+//     //     name: header + job,
+//     //     description: 'test'
+//     //   ).build()
+//     // }
+//     pipelineJob(name) {
+//       definition {
+//         cpsScm {
+//           scm {
+//             git {
+//               remote {
+//                 url(giturl)
+//               }
+//               branch('*/master')
+//             }
+//           }
+//           lightweight()
+//         }
+//       }
+//     }
+//   }
+// }
 parallel tasks
 
 class BaseJobBuilder {
