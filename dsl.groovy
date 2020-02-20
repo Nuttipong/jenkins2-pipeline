aa920acfca4034ae49c49bd23059029b959084f2
nuttipong.t <nuttipongg@gmail.com>
2020-02-21 00:33:31 +0700

    feat: modify scriptclear

--- dsl.groovy
+++ dsl.groovy
@@ -175,7 +175,7 @@ class Mobile implements AddChoiceParam, AddConfig, AddStringParam, AddDefinition
   void build() {
       this.addLogRotator(100)
       this.addChoiceParam("ENVIRONMENT", ["maint", "maint" + "@AWS"], "")
-      this.addChoiceParam("M_APP_VERSION", ["4.0.1", "4.0.0", "tell Code-Push apply to which mobile package version")
+      this.addChoiceParam("M_APP_VERSION", ["4.0.1", "4.0.0"], "tell Code-Push apply to which mobile package version")
       this.addChoiceParam("PATCHING_OPTION", ["patch", "minor", "major"], "select type of version patching (major.minor.patch)")
       this.addStringParam("BUILD_SPECIFIER", "", "input Branch-name or Tag-name or Commit-id")
       this.addDefinition("hexalite/integrated_app2019", "refs/remotes/${defaultBranch}", false, "jenkins-script/Jenkinsfile_release_3.5.groovy")
