1db074b84ec9b91256b9702e7f4ff252ce20625a
nuttipong.t <nuttipongg@gmail.com>
2020-02-21 01:26:10 +0700

    feat: modify scriptclear

--- dsl.groovy
+++ dsl.groovy
@@ -174,7 +174,7 @@ class Mobile extends BaseJob implements Job {
       this.addChoiceParam("M_APP_VERSION", ["4.0.1", "4.0.0"], "tell Code-Push apply to which mobile package version")
       this.addChoiceParam("PATCHING_OPTION", ["patch", "minor", "major"], "select type of version patching (major.minor.patch)")
       this.addStringParam("BUILD_SPECIFIER", "", "input Branch-name or Tag-name or Commit-id")
-      this.addDefinition("hexalite/integrated_app2019", "refs/remotes/master", false, "jenkins-script/Jenkinsfile_release_3.5.groovy")
+      //this.addDefinition("hexalite/integrated_app2019", "refs/remotes/master", false, "jenkins-script/Jenkinsfile_release_3.5.groovy")
   }
 }
 
