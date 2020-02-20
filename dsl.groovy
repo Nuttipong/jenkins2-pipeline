c4a72d17216e86315e83bef3244bc3d5ea95e27f
nuttipong.t <nuttipongg@gmail.com>
2020-02-20 12:19:57 +0700

    feat: modify script

--- dsl.groovy
+++ dsl.groovy
@@ -8,86 +8,86 @@ def jobs = [
 ]
 def tasks = [:]
 
-jobs.each {
-  name -> tasks[name] = {
-    // new BaseJobBuilder(
-    //   name: header + job,
-    //   description: 'test'
-    // ).build()
-    //node {
-      pipelineJob(name) {
-          wrappers {
-              colorizeOutput()
-              timestamps()
-          }
-          logRotator {
-              numToKeep(100)
-          }
-      }
-    //}
-  }
-}
-parallel tasks
-
-class BaseJobBuilder {
-    String name
-    String description
-
-    def build() {
-      pipelineJob(this.name) {
-        CommonUtils.addDefaults(this)
-      }
-    }
-}
-
-class CommonUtils {
-  static void addDefaults(context) {
-      context.with {
-          wrappers {
-              colorizeOutput()
-              timestamps()
-          }
-          logRotator {
-              numToKeep(100)
-          }
-          definition {
-            cpsScm {
-              scm {
-                git {
-                  remote {
-                    url(giturl)
-                  }
-                  branch('*/master')
-                }
-              }
-              lightweight()
-            }
-          }
-      }
-  }
-}
-
-// parallel firstBranch: {
-//         println new Date() - started
-//     }, secondBranch: {
-
-//         println new Date() - started
-//     },
-//     failFast: true
-// }
-
-// pipelineJob('job-dsl-plugin') {
-//   definition {
-//     cpsScm {
-//       scm {
-//         git {
-//           remote {
-//             url(giturl)
+// jobs.each {
+//   name -> tasks[name] = {
+//     // new BaseJobBuilder(
+//     //   name: header + job,
+//     //   description: 'test'
+//     // ).build()
+//     //node {
+//       pipelineJob(name) {
+//           wrappers {
+//               colorizeOutput()
+//               timestamps()
 //           }
-//           branch('*/master')
-//         }
+//           logRotator {
+//               numToKeep(100)
+//           }
 //       }
-//       lightweight()
-//     }
+//     //}
 //   }
 // }
+// parallel tasks
+
+// class BaseJobBuilder {
+//     String name
+//     String description
+
+//     def build() {
+//       pipelineJob(this.name) {
+//         CommonUtils.addDefaults(this)
+//       }
+//     }
+// }
+
+// class CommonUtils {
+//   static void addDefaults(context) {
+//       context.with {
+//           wrappers {
+//               colorizeOutput()
+//               timestamps()
+//           }
+//           logRotator {
+//               numToKeep(100)
+//           }
+//           definition {
+//             cpsScm {
+//               scm {
+//                 git {
+//                   remote {
+//                     url(giturl)
+//                   }
+//                   branch('*/master')
+//                 }
+//               }
+//               lightweight()
+//             }
+//           }
+//       }
+//   }
+// }
+
+// // parallel firstBranch: {
+// //         println new Date() - started
+// //     }, secondBranch: {
+
+// //         println new Date() - started
+// //     },
+// //     failFast: true
+// // }
+
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url(giturl)
+          }
+          branch('*/master')
+        }
+      }
+      lightweight()
+    }
+  }
+}
