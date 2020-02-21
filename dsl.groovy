81b3b737edc390c39b3f67718a1ebe6e1f0011ae
nuttipong.t <nuttipongg@gmail.com>
2020-02-21 17:35:04 +0700

    feat: modify script

--- dsl.groovy
+++ dsl.groovy
@@ -2,36 +2,39 @@
 import jobs.Portal
 import jobs.IA as Ia
 import jobs.Backend
-import jobs.Mock
 import logic.Job
 
-final header = "hxl_maint_4.0_"
-final headerToMock = "hxl_maint_4.0_to_abs_mock"
+/*
+*  ===========================================================
+*                           MAINT
+*  ===========================================================
+*/
 
 def tasks = [
-  portal: ["provider_portal", "", Portal, false],
-  ia: ["integrated_app", "", Ia, false],
-  // portal: ["provider_portal_to_mock", "", Portal, true],
-  // ia: ["integrated_app_to_mock", "", Ia, true],
-  // apiGatewayMock: ["gbmf-apigateway-mock", "", Backend],
-  // gbmfConfigServer: ["gbmf-config-server", "", Backend],
-  // gbmfServiceDiscovery: ["gbmf-service-discovery", "", Backend],
-  // gbmfFleetManagement: ["gbmf-fleet-management", "", Backend],
-  // gbmfGeoTracking: ["gbmf-geo-tracking", "", Backend],
-  // gbmfGuac: ["gbmf-guac", "", Backend],
-  // gbmfNotification: ["gbmf-notification", "", Backend],
-  // gbmfServiceBroker: ["gbmf-service-broker", "", Backend],
-  // gbmfJobMonitoring: ["gbmf-job-monitoring", "", Backend],
-  // gbmfTranslation: ["gbmf-translation", "", Backend],
-  // gbmfUserManagement: ["gbmf-user-management", "", Backend],
-  // gbmfJobMonitoring: ["gbmf-job-monitoring", "", Mock],
-  // gbmfServiceBroker: ["gbmf-service-broker", "", Mock],
+  portal: ["provider-portal", "", Portal, false],
+  ia: ["integrated-app", "", Ia, false],
+  portalMock: ["provider-portal-mock", "", Portal, true],
+  iaMock: ["integrated-app-mock", "", Ia, true],
+  apiGatewayMock: ["gbmf-apigateway-mock", "", Backend, false],
+  gbmfConfigServer: ["gbmf-config-server", "", Backend, false],
+  gbmfServiceDiscovery: ["gbmf-service-discovery", "", Backend, false],
+  gbmfFleetManagement: ["gbmf-fleet-management", "", Backend, false],
+  gbmfGeoTracking: ["gbmf-geo-tracking", "", Backend, false],
+  gbmfGuac: ["gbmf-guac", "", Backend, false],
+  gbmfNotification: ["gbmf-notification", "", Backend, false],
+  gbmfServiceBroker: ["gbmf-service-broker", "", Backend, false],
+  gbmfJobMonitoring: ["gbmf-job-monitoring", "", Backend, false],
+  gbmfTranslation: ["gbmf-translation", "", Backend, false],
+  gbmfUserManagement: ["gbmf-user-management", "", Backend, false],
+  gbmfJobMonitoringMock: ["gbmf-job-monitoring-mock", "", Backend, true],
+  gbmfServiceBrokerMock: ["gbmf-service-broker-mock", "", Backend, true]
 ]
+
+def moduleList = ["not-build"]
 
 tasks.values().each {
   task -> 
-      final header = Mock == task[2] ? headerToMock + task[0] : header + task[0]
-      def pipeline = pipelineJob()
+      def pipeline = pipelineJob(task[0])
       Job job
       switch(task[2]) {
         case Portal:
@@ -45,13 +48,14 @@ tasks.values().each {
         case Backend:
           job = new Backend(pipeline)
           job.name = task[0]
+          job.isMock = task[3]
+          moduleList.push(task[0])
         break;
-        case Mock:
-          job = new Mock(pipeline)
-          job.name = task[0]
+        case Promregator:
+          job = new Promregator(pipeline)
+          moduleList.push(task[0])
         break;
       } 
       job.build()
 }
-
 
